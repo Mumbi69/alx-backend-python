26d178923c73f857ad69093a7137a7a6160aeb21
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-15 21:22:34 +0300

    Measure the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 """Measure the runtime"""
 
+
 import asyncio
 import time
 from typing import List
 from concurrent.futures import ProcessPoolExecutor
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 async def wait_n_async(n: int, max_delay: int = 10) -> List[float]:
     return await wait_n(n, max_delay)
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """
-    Measures the total execution time for wait_n(n, max_delay) and returns total_time / n.
+    Measures the total execution time for wait_n(n, max_delay)
+    and returns total_time / n.
 
     Parameters:
         n (int): Number of times to spawn wait_random.
@@ -30,6 +35,7 @@ def measure_time(n: int, max_delay: int) -> float:
     total_time = time.time() - start_time
 
     return total_time / n
+
 
 if __name__ == "__main__":
     n = 5
